1407ca879b564678c06be5bdb1bc1dc9a2665e04
NovikovViktor <viktor.novikov95@gmail.com>
2015-12-13 22:51:32 +0200

    Configure form type to be compatible with Symfony => 2.7

--- Form/Type/CronType.php
+++ Form/Type/CronType.php
@@ -4,7 +4,7 @@ namespace FOA\CronBundle\Form\Type;
 
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Cron job form type
@@ -31,7 +31,7 @@ class CronType extends AbstractType
             ));
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
             'data_class' => 'FOA\CronBundle\Manager\Cron'
